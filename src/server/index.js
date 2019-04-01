7cd51bd16a3dbf2da76adc4c2e6699be312d2f9b
mushon <mushon4@gmail.com>
2019-04-01 19:43:27 +0300

    first commit

---
+++ src/server/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const os = require('os');
+const sheetsAuth = require('./sheetsAuth.js');
+const { google } = require('googleapis');
+
+// const app = express();
+
+// app.use(express.static('dist'));
+// app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username + sheetsResolver.returnData }));
+
+// app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
+
+/**
+ * Prints the names and majors of students in a sample spreadsheet:
+ * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
+ */
+function getCellRangeInSheet(auth, args) {
+  const sheets = google.sheets({ version: 'v4', auth });
+  sheets.spreadsheets.values.get(args, (err, res) => {
+    if (err) return console.log('The API returned an error: ' + err);
+    const rows = res.data.values;
+    if (rows.length) {
+      console.log(rows);
+      return rows;
+    } else {
+      console.log('No data found.');
+    }
+  });
+}
+
+var args = new Object();
+args = {
+  spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
+  range: 'Class Data!A2:E'
+};
+
+const rows = sheetsAuth.authorize(getCellRangeInSheet, args);
+// console.log('sds');
+// console.log(rows);
+// rows.map(row => {
+//   console.log(`${row[0]}, ${row[4]}`);
+// });
